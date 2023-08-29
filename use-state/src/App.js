94abbb627c56c9e38d5ce84577ff1a9d57979aa9
Jyoti Manglani <manglanijyoti62@gmail.com>
2023-08-29 22:25:51 +0530

    counter-2 using useState

--- use-state/src/App.js
+++ use-state/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import HookCounter from './component/HookCounter';
+import HookCounter2 from './component/HookCounter2';
 function App() {
   
   return (
     <div className="App">
-      <HookCounter/>
+      {/* <HookCounter/> */}
+      <HookCounter2/>
     </div>
   );
 }
---
+++ use-state/src/component/HookCounter2.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function HookCounter2() {
+    const initialCount = 0 
+    const [count, setCount] = React.useState(initialCount)
+  return (
+    <div>
+        <h1>Counter-2</h1>
+        <h2>Count : {count}</h2>
+        <button onClick={() => setCount(initialCount)}>Reset</button>
+        <button onClick={() => setCount(prevCount => prevCount + 1)}>Increment</button>
+        <button onClick={() => setCount(prevCount => prevCount - 1)}>Decrement</button>
+    </div>
+  )
+}
+
+export default HookCounter2
