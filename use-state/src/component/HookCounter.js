5e563c4caf97fcc489a6480b729bd732aa0e43ab
Jyoti Manglani <manglanijyoti62@gmail.com>
2023-08-29 21:56:32 +0530

    updated css

--- use-state/src/component/HookCounter.js
+++ use-state/src/component/HookCounter.js
@@ -4,6 +4,7 @@ function HookCounter() {
     const[count, setCount] = useState(0)
   return (
     <div>
+        <h1>Counter</h1>
         <button onClick={() => setCount(count+1)}>Count {count}</button>
     </div>
   )
