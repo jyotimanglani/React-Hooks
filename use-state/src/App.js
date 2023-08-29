c464a7e7450a401929443c9be9ce0f0041a7fef6
Jyoti Manglani <manglanijyoti62@gmail.com>
2023-08-29 21:41:23 +0530

    counter using useState completed

---
+++ use-state/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HookCounter from './component/HookCounter';
+function App() {
+  
+  return (
+    <div className="App">
+      <HookCounter/>
+    </div>
+  );
+}
+
+export default App;
