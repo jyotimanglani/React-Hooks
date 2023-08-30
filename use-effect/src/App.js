473f9bfb37f650febfe7879541995f16acc12ca7
Jyoti Manglani <manglanijyoti62@gmail.com>
2023-08-30 20:53:15 +0530

    run effects only once done

--- use-effect/src/App.js
+++ use-effect/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import HookCounterOne from './components/HookCounterOne';
+import HookMouse from './components/HookMouse';
 
 function App() {
   return (
     <div className="App">
       <h1>useEffect hook</h1>
         <HookCounterOne/>
+        <HookMouse/>
     </div>
   );
 }
