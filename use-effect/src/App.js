6a0a6fc692df8f06e9ee39989f9b1c905ecb5ca8
Jyoti Manglani <manglanijyoti62@gmail.com>
2023-08-30 20:33:40 +0530

    conditionally run effects using useEffect done

--- use-effect/src/App.js
+++ use-effect/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import HookCounterOne from './components/HookCounterOne';
 
 function App() {
   return (
     <div className="App">
+      <h1>useEffect hook</h1>
         <HookCounterOne/>
     </div>
   );
--- use-effect/src/components/HookCounterOne.js
+++ use-effect/src/components/HookCounterOne.js
@@ -2,12 +2,16 @@ import React, { useEffect } from 'react'
 
 function HookCounterOne() {
     const [count, setCount] = React.useState(0)
+    const [name, setName] = React.useState('')
 
     useEffect(()=> {
-        document.title = `You clicked ${count} times`
-    })
+        console.log("updating")
+        document.title = `You clicked ${count} times` //useEffect after render
+    },[count]) //conditionally run effects 
+
   return (
     <div>
+        <input type='text' onChange={e => setName(e.target.value)}></input>
         <button onClick={()=>setCount(count+1)}>Clicked {count} times</button>
     </div>
   )
