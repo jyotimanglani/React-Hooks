9fa49bd1842c881b4a4f48dd31a6dfa0fce01b7d
Jyoti Manglani <manglanijyoti62@gmail.com>
2023-09-03 15:54:43 +0530

    multiple use reducers done

---
+++ use-reducer-hook/src/components/CounterThree.js
@@ -0,0 +1,40 @@
+import userEvent from '@testing-library/user-event'
+import React, { useReducer } from 'react'
+
+ const initialState = 0
+ const reducer = (state, action) =>{
+    switch(action){
+        case 'increment':
+            return state+1
+        case 'decrement':
+            return state-1
+        case 'reset':
+            return initialState
+        default:
+            return state
+    }
+ }
+
+function CounterThree() {
+ const [count, dispatch] = useReducer(reducer, initialState)
+ const [countTwo, dispatchTwo] = useReducer(reducer, initialState)
+  return (
+    <div>
+        <h1>use Reducer Hook </h1>
+        <p>(multiple useReducers)</p>
+        <div>Count - {count}</div>
+        <button onClick={() => dispatch('increment')}>Increment</button>
+        <button onClick={() => dispatch('decrement')}>Decrement</button>
+        <button onClick={() => dispatch('reset')}>Reset</button>
+
+        <div>
+        <div>Count Two - {countTwo}</div>
+        <button onClick={() => dispatchTwo('increment')}>Increment</button>
+        <button onClick={() => dispatchTwo('decrement')}>Decrement</button>
+        <button onClick={() => dispatchTwo('reset')}>Reset</button>
+        </div>
+    </div>
+  )
+}
+
+export default CounterThree
