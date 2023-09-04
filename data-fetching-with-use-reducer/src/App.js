0b80630a429a9999911088e7865d2817a52fa1b9
Jyoti Manglani <manglanijyoti62@gmail.com>
2023-09-04 21:00:04 +0530

    data fetching using useReducer done

---
+++ data-fetching-with-use-reducer/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import DataFetchingOne from './components/DataFetchingOne';
+
+function App() {
+  return (
+    <div className="App">
+      <DataFetchingOne/>
+    </div>
+  );
+}
+
+export default App;
