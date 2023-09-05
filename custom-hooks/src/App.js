3f15bcbfe7c05284f1656f74000ea264d6c03451
Jyoti Manglani <manglanijyoti62@gmail.com>
2023-09-05 21:41:56 +0530

    custom hook - useCounter hook done

--- custom-hooks/src/App.js
+++ custom-hooks/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import DocTitleOne from './components/DocTitleOne';
+import Counter from './components/counter';
 
 function App() {
   return (
     <div className="App">
-     <DocTitleOne/>
+     {/* <DocTitleOne/> */}
+     <Counter/>
     </div>
   );
 }
